9d5bcd8e486afe696a68223672999808a7b01aac
Santiago Acosta <saacostad@unal.edu.co>
2025-06-12 20:54:14 -0500

    Second version: still not working

--- ErrSelector.py
+++ ErrSelector.py
@@ -23,8 +23,22 @@ selectedQP = selQP().iloc[
 
 
 """
+        GLOBAL PARAMETERS 
+"""
+
+
+# If the system of equations, aftereach iteration, don't change in more than
+# this quantity, then we're satisfied
+TRESHOLD = 0.8e-5
+
+
+# This is the error we're gonna try to recreate
+ERRs = np.array(list(range(1, 9))) * 10 ** (-7)
+
+"""
     ACTION AND PHASE FOR EACH IP
 The nominal action and phase are pretty much constants on each side of IP, so, we bring them here manually
+NOTE: at first, this doesn't matter, but who knows if in the future they will
 """
 
 HJ0s = [1.57318230159e-10, 1.59040817156e-10, 1.93292075956e-11, 4.33351930746e-11]
@@ -32,18 +46,44 @@ VJ0s = [1.09287769985e-13, 1.07739134429e-13, 9.28858562765e-10, 9.45894676299e-
 HP0s = [0.587739114054, 0.588883553621, -2.15738114587, 2.479568333]
 VP0s = [1.58772647432, 1.59059853389, 1.20002337969, -0.520781188437]
 
-
-# This is the error we're gonna try to recreate
-ERRs = np.array(list(range(1, 9))) * 10 ** (-5)
-
-
 """
     FUNCTION DEFINITIONS
 In this part, we define different functions to make the script more readable
 """
 
 
-def linearTermMatrix(quadrupoles):
+def createRowKL(df):
+    """This function takes a pandas dataFrame df, creates a new column KL and, according to the name of its
+    quadrupoles, appends the value of the magnetic kick of the quadrupoles"""
+
+    df["KL"] = np.nan  # We create the new row in the dataframe
+
+    for index, row in df.iterrows():
+        qpName = row["NAME"].split(".")[0]
+        df.at[index, "KL"] = physParams.loc[physParams["NAME"] == qpName, "KL"].values[
+            0
+        ] * 10 ** (-4)
+
+
+"""
+    DATA MODIFICATION
+In this part, we modify the original quadrupoles dataframes SO they contain the new column KL, which will be used 
+in order to calculate the magnetic errors of the quadrupoles
+"""
+
+for index, row in selectedQP.iterrows():
+    createRowKL(selectedQP.loc[index, "leftX"])
+    createRowKL(selectedQP.loc[index, "leftY"])
+    createRowKL(selectedQP.loc[index, "rightX"])
+    createRowKL(selectedQP.loc[index, "rightY"])
+
+
+"""
+    MATRIX CREATION
+"""
+
+
+def firstOrderMatrix(quadrupoles):
     """This function takes una region number and creates the matrix from which one can get the linear terms"""
 
     # left = selectedQP.loc[reg - 1, "leftX"]
@@ -94,130 +134,67 @@ def secondOrderVector(qp, err=ERRs):
 
     # Now, we multiply the matrix by the errors values
     secondOrderVector = nonLinearMatrix @ err
-    print(qp)
+
     # Finally, we have to return the convolution of the linear and the second order vectors
     return linVector * secondOrderVector
 
 
-left = selectedQP.loc[0, "leftX"]
-right = selectedQP.loc[0, "rightX"]
-
-quadrupoles = pd.concat([left, right])
-
-print(secondOrderVector(quadrupoles))
+"""
+        EQUATION SYSTEM 
+"""
 
 
-def ErrorSimulation(errors, reg):
+def ErrorSimulation(quadrupoles, errors=ERRs):
     """Takes a vector of errors that will be simulated on the 8 quadrupoles and
     outputs the value of the 2nd order equation for error"""
+
+    firstOrderTerm = firstOrderMatrix(quadrupoles) @ errors
+
+    secondOrderTerm = secondOrderVector(quadrupoles, errors)
+
+    return firstOrderTerm + secondOrderTerm
+
+
+def findLinearSolutions(quadrupoles, solution):
+    """Function that, given an interaction region _reg_, performs all the code to find the first approximations to
+    the solutions of the errors, that is, solves the system of linear non-cross equations"""
+
+    A = firstOrderMatrix(quadrupoles)
+
+    Corrections = np.linalg.solve(A, solution)
+
+    return Corrections
+
+
+def findSystemSolution(reg, errors=ERRs, treshold=TRESHOLD):
+    """This function runs the iterative method for finding a solution of the system"""
+
+    print(f"Simulated Errors: {errors}")
 
     left = selectedQP.loc[reg - 1, "leftX"]
     right = selectedQP.loc[reg - 1, "rightX"]
 
     quadrupoles = pd.concat([left, right])
 
-    print(quadrupoles)
+    rightSideConstants = ErrorSimulation(quadrupoles, errors)
 
-    firstOrderTerm = sum(
-        quadrupoles.apply(
-            lambda qp: qp["KL"] * qp["BETX"] * sin(qp["MUX"]) * cos(qp["MUX"]), axis=1
-        )
-    )
+    corrections = findLinearSolutions(quadrupoles, rightSideConstants)
 
-    return firstOrderTerm
+    print("First order corrections: ")
+    print(corrections)
+    # secondOrderConstantTerm = secondOrderVector(quadrupoles, errors)
 
+    i = 1
+    while True:
+        secondOrderTerm = secondOrderVector(quadrupoles, errors - corrections)
+        rightSide = rightSideConstants - secondOrderTerm
 
-def createRowKL(df):
-    """This function takes a pandas dataFrame df, creates a new column KL and, according to the name of its
-    quadrupoles, appends the value of the magnetic kick of the quadrupoles"""
+        corrections = findLinearSolutions(quadrupoles, rightSide)
 
-    df["KL"] = np.nan  # We create the new row in the dataframe
+        print(f"Iteration {i}")
+        print(corrections)
 
-    for index, row in df.iterrows():
-        qpName = row["NAME"].split(".")[0]
-        df.at[index, "KL"] = physParams.loc[physParams["NAME"] == qpName, "KL"].values[
-            0
-        ] * 10 ** (-4)
+        i += 1
 
 
-"""
-    DATA MODIFICATION
-In this part, we modify the original quadrupoles dataframes SO they contain the new column KL, which will be used 
-in order to calculate the magnetic errors of the quadrupoles
-"""
-
-for index, row in selectedQP.iterrows():
-    createRowKL(selectedQP.loc[index, "leftX"])
-    createRowKL(selectedQP.loc[index, "leftY"])
-    createRowKL(selectedQP.loc[index, "rightX"])
-    createRowKL(selectedQP.loc[index, "rightY"])
-
-
-"""
-    MATRIX CREATION
-"""
-
-
-def diagBetaMatrix(reg):
-    """Creates a diagonal matrix with the beta functions of each quadrupole of the IR. It is used
-    as a factor in the equation, taking into account the beta_i can be factorized out"""
-
-    left = selectedQP.loc[reg - 1, "leftX"]
-    right = selectedQP.loc[reg - 1, "rightX"]
-
-    return np.diag(list(left.loc[:, "BETX"]) + list(right.loc[:, "BETX"]))
-
-
-def diagWeightedMatrix(reg):
-    """Same thing as previous function but with the weights of each quadrupole for the linear expressions"""
-
-    left = list(selectedQP.loc[reg - 1, "leftX"].loc[:, "MUX"])
-    right = list(selectedQP.loc[reg - 1, "rightX"].loc[:, "MUX"])
-
-    MUs = left + right
-    DELTA0 = HP0s[reg - 1]
-
-    firstMatrix = (
-        np.diag([cos(MUs[i]) * sin(MUs[i]) for i in range(0, 8)])
-        * cos(DELTA0)
-        * sin(phi_s)
-    )
-    secondMatrix = (
-        np.diag([-sin(MUs[i]) * sin(MUs[i]) for i in range(0, 8)])
-        * cos(DELTA0)
-        * cos(phi_s)
-    )
-    thirdMatrix = (
-        np.diag([-cos(MUs[i]) * cos(MUs[i]) for i in range(0, 8)])
-        * sin(DELTA0)
-        * sin(phi_s)
-    )
-    fourthMatrix = (
-        np.diag([sin(MUs[i]) * cos(MUs[i]) for i in range(0, 8)])
-        * sin(DELTA0)
-        * cos(phi_s)
-    )
-
-    return firstMatrix + secondMatrix + thirdMatrix + fourthMatrix
-
-
-def linearMatrix(reg):
-    """This function returns a matrix that corresponds to the one used to solve the linear terms,
-    ignoring the cross terms"""
-    betaMatrix = diagBetaMatrix(reg)
-    weightMatrix = diagWeightedMatrix(reg)
-
-    return betaMatrix * weightMatrix
-
-
-# Solution vector
-def findLinearSolutions(reg):
-    """Function that, given an interaction region _reg_, performs all the code to find the first approximations to
-    the solutions of the errors, that is, solves the system of linear non-cross equations"""
-
-    A = linearMatrix(reg)
-    Sol = [sin(phi_s - HP0s[reg - 1])] * 8
-
-    Corrections = np.linalg.solve(A, Sol)
-
-    return Corrections
+findSystemSolution(1, errors=ERRs)
