7e044bcc827bc7ecadb768ab4d4e08adbe173b06
Santiago A <saacostad@unal.edu.co>
2026-02-23 14:45:02 -0500

    Matricial formulation code. TODO: implement Neumman inverse

---
+++ MatricialFunction.py
@@ -0,0 +1,78 @@
+import numpy as np 
+
+
+
+def CreateQ(B, P, K, p, grad):
+    """
+    This function creates the Q matrix given that 
+
+    \hat Q = \hat{\Delta K} ( 1 - p \hat{M} \hat{\Delta K} )^{-1}
+
+    with 
+
+    \hat{\Delta K} = \Diag(\Delta K_i)
+    M_{ij} = \sin(\psi_i - \psi_j) \sqrt(\beta_j \beta_i) for j < i else 0
+
+    where grad is the "order of the inverse" of the term in parenthesis, given by truncating 
+    the Neumman series of p \hat{M} \hat{\Delta K} when 
+
+    ( 1 - p \hat{M} \hat{\Delta K} )^{-1} = \sum_{k = 1}^{grad} (p \hat{M} \hat{\Delta K} )^k
+    """
+
+    # Matrix form of \Delta K
+    hatK = np.diag(K)
+    
+
+    # We create the matrix M 
+    #-------------------------------
+
+    n = len(P)  # Number of Quadrupoles
+    
+    hatM = np.zeros((n, n))    # Create a square matrix first
+
+    i, j = np.tril_indices(n, k=-1) # Take only the indexes of the lower triangular section
+    
+    hatM[i, j] = np.sin(P[i] - P[j]) * np.sqrt(B[i] * B[j])    # Fill the gaps with what we need 
+
+
+    # Now we create the whole matrix in parenthesis and we invert it 
+    # TODO: make the Neumman inversion method
+    #---------------------------------------------------------------------------
+
+    Q = hatK @ np.linalg.inv(np.eye(n) - p * hatM @ hatK)
+
+    return Q
+
+    
+
+def CreateSystem(B, P, K, p = 1, grad = 8):
+    """
+    This function takes the system's \beta's, \phi's and quadrupole magnetic errors \Delta K
+    and returns a tuple with the matrix \hat Q, and the vectors \vec u and \vec v following that
+
+    \vec u = {\sqrt(\beta_i) \sin(\psi_i)}
+    \vec v = {\sqrt(\beta_i) \cos(\psi_i)}
+
+    \hat Q = \hat{\Delta K} ( 1 - p \hat{M} \hat{\Delta K} )^{-1}
+    """
+
+    u = np.sqrt(B) * np.sin(P)
+    v = np.sqrt(B) * np.cos(P)
+
+    Q = CreateQ(B, P, K, p, grad)
+
+    return Q, u, v
+
+
+def CreateConstants(Q, u, v):
+    """
+    This function takes the system vectors/matrices and calculates the coefficients 
+    of the expansion we care about. Returns a tuple with 
+
+    \vec v^T \hat Q \vec u
+    \vec v^T \hat Q \vec v
+    \vec v^u \hat Q \vec u
+    \vec v^u \hat Q \vec v
+    """
+
+    return(v @ Q @ u, v @ Q @ v, u @ Q @ u, u @ Q @ v)
